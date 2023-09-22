0a53b4a4490d689c8341dc70552324c13e24eaf6
511NetworkAuthenticationRequired <143905058+511NetworkAuthenticationRequired@users.noreply.github.com>
2023-09-22 01:44:45 -0300

    Update AR_322.go

--- Arreglos/3.22/AR_322.go
+++ Arreglos/3.22/AR_322.go
@@ -2,7 +2,7 @@ ACCION 3.22 ES
 	AMBIENTE
 		// VARIABLES & CONSTANTES
 		x; y: entero
-		suma_fila; suma_columna; total: entero
+		suma_fila; suma_columna: entero
 		// ARREGLOS & MATRICES
 		entrada: arreglo de [1...5, 1...5] de enteros
 		salida: arreglo de [1...2, 1...5] de enteros
@@ -16,7 +16,6 @@ ACCION 3.22 ES
 		Para (x:=1 hasta 5) Hacer
 			Para (y:= 1 hasta 5) Hacer
 				suma_columna:= suma_columna + entrada[x, y]
-				y:= y
 			FinPara
 			salida[2, y]:= suma_columna
 			y:= 0
@@ -25,7 +24,6 @@ ACCION 3.22 ES
 		Para (y:=1 hasta 5) Hacer
 			Para (x:=1 hasta 5) Hacer
 				suma_fila:= suma_fila + entrada[x, y]
-				x:= x
 			FinPara
 			salida[1, x]:= suma_fila
 			x:= 0
